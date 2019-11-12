8b364674da0f369cb9739a86bf35b96e9be4bf2d
Dunn <e_dunn112735@ats-hr420-dm32.fc.ca>
2019-11-12 13:55:31 -0500

    updated campoariosns

--- game.py
+++ game.py
@@ -26,40 +26,15 @@ while gamedunn.player is False:
 	print("computer chose ", gamedunn.computer, "\n")
 	print("player chose ", player, "\n")
 
-	if player == "quit":
-		exit()
-	elif gamedunn.computer == player:
-		print("tie! no one wins, play again")
-
-	elif player.lower()=="rock":
-		if gamedunn.computer == "paper":
-			print("you lose :( ", gamedunn.computer, "covers", player, "\n")
-			player_lives = player_lives - 1
-		else:
-			print("you win :) ", player, "smashes", gamedunn.computer, "\n")
-			gamedunn.computer_lives = gamedunn._lives - 1
-
-	elif player.lower() =="paper":
-		if gamedunn.computer == "scissors":
-			print("you lose :( ", gamedunn.computer, "cuts", player, "\n")
-			player_lives = player_lives - 1
-		else:
-			print("you win :) ", player, "smashes", gamedunn.computer, "\n")
-			gamedunn.computer_lives = gamedunn.computer_lives - 1
-
-	elif player.lower() =="scissors":
-		if gamedunn.computer == "rock":
-			print("you lose :( ", gamedunn.computer, "smashes", player, "\n")
-			player_lives = player_lives - 1
-		else:
-			print("you win :) ", player, "cuts", gamedunn.computer, "\n")
-			gamedunn.computer_lives = gamedunn.computer_lives - 1
-
-	else:
-		print("That's not a valid option, try again")
+	### this is where you would call compare
+	comparechoices():
+		print(comparechoices)
+	
+	###end compare stuff
 
 #handle all lives lost for player or AI 
-	if player_lives is 0:
+	if gamedunn.player_lives is 0:
+		winlose.winorlose("lost")
 		print("Your ran out of lives you stupid bitch, play much? loser. play again fuckass?")
 		choice = input ("Y/N")
 		print(choice)
@@ -70,9 +45,9 @@ while gamedunn.player is False:
 
 		elif (choice is "Y") or (choice is "y"):
 			#reset the game so that we can start all over again 
-			player_lives = 5 
+			gamedunn.player_lives = 5 
 			gamedunn.computer_lives = 5 
-			player = False 
+			gamedunn.player = False 
 			gamedunn.computer = choices[randint(0,2)]
 
 	elif gamedunn.computer_lives is 0:
